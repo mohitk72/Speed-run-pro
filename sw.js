46e236d6781a46ecc4bde35908baf289a529d1c6
mohitk72 <mohitkale782@gmail.com>
2026-02-23 10:26:05 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,17 @@
+const CACHE_NAME = 'speedrun-max-v1';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png',
+  './splash.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.min.js'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+});
+
 self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
